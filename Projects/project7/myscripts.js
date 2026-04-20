5b0d8ec9d6dc96f60ef493839f461fae70419a02
Javi Lopez Ramirez <157434915+customerserviceee@users.noreply.github.com>
2026-04-20 17:30:47 -0600

    Project 7

---
+++ Projects/project7/myscripts.js
@@ -0,0 +1,54 @@
+PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	PrintButton1.src = "Images/print5.gif"
+	PrintButton2.src = "Images/print6.gif"
+	logo1.src = "Images/logo1.jpg"
+	logo2.src = "Images/logo2.jpg"
+}
+
+
+		var imgArray = new Array(
+			'Image1Lg.jpg',
+			'Image2Lg.jpg',
+			'Image3Lg.jpg',
+			'Image4Lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'Chef',
+			'Dutch oven',
+			'Couple cooking',
+			'Rasberry Cake '
+			
+		);
+			
+		var imgPath = "Images/Fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
